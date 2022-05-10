83956bd4d8acf6ff4d68e25e1b9ec0e6634f4868
Sean Martin <sean@seanmartinstudio.com>
2022-05-10 12:47:29 -0700

    Complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,38 @@
 class Dog
 
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+end
+
+class Person
+
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def job=(job)
+        @job = job
+    end
 end
